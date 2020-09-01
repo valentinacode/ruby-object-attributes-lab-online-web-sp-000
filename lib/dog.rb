6a94c88e800cd166b02d4819736ff42c4c73936c
Valentina E <tiyahelfrih@gmail.com>
2020-09-01 14:25:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,9 @@
-class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+class Dog 
+  def initialize(name)
+    @name = name
   end
  
   def name
-    @this_dogs_name
+    @name
   end
 end
- 
-lassie = Dog.new
-lassie.name = "Lassie"
- 
-puts lassie.name
